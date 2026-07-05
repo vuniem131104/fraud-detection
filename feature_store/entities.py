1641ae24aaf27140ee3f06c23832589d15da93d0
vuniem131104 <niemhoalongvu@gmail.com>
2026-07-05 23:17:38 +0700

    haha

--- feature_store/entities.py
+++ feature_store/entities.py
@@ -1,11 +1,4 @@
-from feast import Entity
+from feast import Entity, ValueType
 
-user = Entity(
-    name="user_id",
-    join_keys=["user_id"],
-)
-
-card = Entity(
-    name="card_id",
-    join_keys=["card_id"],
-)
+user = Entity(name="user_id", join_keys=["user_id"], value_type=ValueType.INT64)
+card = Entity(name="card_id", join_keys=["card_id"], value_type=ValueType.STRING)
